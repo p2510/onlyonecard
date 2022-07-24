5dbeeb43f5000c087febacde6e3744a213d2e5c3
ernestKassi <ernestdimaria17@gmail.com>
2022-07-24 19:13:53 +0000

    un geste un impact

---
+++ src/pages/UnGesteUnImpact.js
@@ -0,0 +1,12 @@
+import DepenseEtProjet from "src/sections/un-geste-un-impact/DepenseEtProjet";
+import Page from "../components/Page";
+
+// ----------------------------------------------------------------------
+
+export default function UnGesteUnImpact() {
+  return (
+    <Page title="Le 1er compte à impact positif | ONLYONE">
+      <DepenseEtProjet />
+    </Page>
+  );
+}
