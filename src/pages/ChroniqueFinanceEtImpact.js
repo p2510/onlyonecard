c523a5d8fda7dacbc0a53d96c0c8f334544907b9
ernestKassi <ernestdimaria17@gmail.com>
2022-07-27 14:44:01 +0000

    finalisation de la  partie chronique finance et impact

---
+++ src/pages/ChroniqueFinanceEtImpact.js
@@ -0,0 +1,12 @@
+import Page from "../components/Page";
+import FinanceEtImpact from "src/sections/chronique-finance-et-impact/FinanceEtImpact";
+
+// ----------------------------------------------------------------------
+
+export default function ChroniqueFinanceEtImpact() {
+  return (
+    <Page title="Chronique finance et impact | ONLYONE">
+      <FinanceEtImpact />
+    </Page>
+  );
+}
