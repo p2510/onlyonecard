645dca7d4311b5b4d91da1c671bed7032c2d4fa3
ernestKassi <ernestdimaria17@gmail.com>
2022-08-20 23:56:25 +0000

    finalisation de la page nos tarifs

--- src/components/organisms/nos-tarifs-et-abonnements/FonctionnalitesPrincipales.js
+++ src/components/organisms/nos-tarifs-et-abonnements/FonctionnalitesPrincipales.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography, Divider } from "@mui/material";
 import { COUTDATA } from "./nosTarifsEtAbonnements.constant";
 
 export default function FonctionnalitesPrincipales() {
@@ -9,7 +9,12 @@ export default function FonctionnalitesPrincipales() {
       <Container>
         <Grid
           rowSpacing={1}
-          sx={{ display: "flex", justifyContent: "space-between", mb: 3 }}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            mr: { md: -5, xs: 3 },
+            mb: 3,
+          }}
         >
           <Grid item xs={6}>
             {" "}
@@ -18,8 +23,8 @@ export default function FonctionnalitesPrincipales() {
             <Grid
               container
               spacing={{
-                md: 16,
-                xs: 2,
+                md: 5,
+                xs: 10,
               }}
             >
               <Grid item xs={6}>
@@ -28,6 +33,7 @@ export default function FonctionnalitesPrincipales() {
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
+                    borderRadius: "25.5px",
                     textAlign: "center",
                     color: "#fff",
                     fontWeight: "bold",
@@ -36,17 +42,17 @@ export default function FonctionnalitesPrincipales() {
                       xs: "45px",
                     },
                     width: {
-                      md: "210px",
+                      md: "292px",
                       xs: "105px",
                     },
                     fontSize: {
-                      md: "20px",
+                      md: "24px",
                       xs: "11px",
                     },
-                    bgcolor: "#00db89",
+                    bgcolor: "#10403B",
                   }}
                 >
-                  Compte One Jeune (18-25 ans)
+                  Compte One Jeune <br /> (18-25 ans)
                 </Typography>
               </Grid>
               <Grid item xs={6}>
@@ -55,6 +61,7 @@ export default function FonctionnalitesPrincipales() {
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
+                    borderRadius: "25.5px",
                     textAlign: "center",
                     color: "#fff",
                     fontWeight: "bold",
@@ -63,22 +70,30 @@ export default function FonctionnalitesPrincipales() {
                       xs: "45px",
                     },
                     width: {
-                      md: "210px",
+                      md: "292px",
                       xs: "105px",
                     },
                     fontSize: {
-                      md: "20px",
+                      md: "24px",
                       xs: "11px",
                     },
-                    bgcolor: "#00db89",
+                    bgcolor: "#10403B",
                   }}
                 >
-                  Compte One Jeune (18-25 ans)
+                  Compte One Jeune <br /> (18-25 ans)
                 </Typography>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
+        <Divider
+          color="#10403B"
+          sx={{
+            height: "2px",
+            mb: { md: 7, xs: 0 },
+            mt: { md: 7, xs: 0 },
+          }}
+        />
         {COUTDATA.map((item) => (
           <Grid
             key={item.id}
@@ -86,8 +101,16 @@ export default function FonctionnalitesPrincipales() {
             spacing={22}
             sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}
           >
-            <Grid item xs={6} fontSize={{ md: "18px", xs: "12px" }}>
-              {item.libelle}
+            <Grid item xs={6}>
+              <Typography
+                sx={{
+                  fontFamily: "Lora",
+                  fontSize: { md: "21px", xs: "12px" },
+                  fontWeight: 600,
+                }}
+              >
+                {item.libelle}
+              </Typography>
             </Grid>
             <Grid item xs={6}>
               <Grid container>
@@ -98,7 +121,8 @@ export default function FonctionnalitesPrincipales() {
                       justifyContent: "start",
                       alignItems: "center",
                       textAlign: "center",
-                      fontWeight: "bold",
+                      fontSize: "24px",
+                      fontWeight: 700,
                       ml: {
                         md: 5,
                         xs: -13,
@@ -115,7 +139,8 @@ export default function FonctionnalitesPrincipales() {
                       justifyContent: "center",
                       alignItems: "center",
                       textAlign: "center",
-                      fontWeight: "bold",
+                      fontSize: "24px",
+                      fontWeight: 700,
                       ml: {
                         md: 6,
                         xs: -6,
